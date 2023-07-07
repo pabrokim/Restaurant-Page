29b9d1d4e5a9bb918b3b48461ca7d1998804c417
Paul Kimani <pabrokimani@gmail.com>
2023-07-07 10:44:58 +0300

    Adds contact data

--- src/contact.js
+++ src/contact.js
@@ -1,22 +1,45 @@
 
 export default function addContact(){
+    let staff = [
+        { photo:'../img/manager.jpg', person:'Paul John', position: 'Manager', phone: '+555-666-777', email:'manager@restaurant' },
+        { photo:'../img/chef.jpg', person: 'Daniel Joel', position: 'Chef', phone: '+555-667-885', email:'cashier@restaurant'},
+        { photo:'../img/customerep.jpeg', person: 'Alicia Grace', position: 'Customer rep', phone: '+555-668-546', email:'customerep@restaurant'},
+    ];
+    for(let i=0; i <staff.length; i++){
+        
+        let myStaff = staff[i];
+     
 
-    const contactContainer = document.createElement('div');
-    contactContainer.classList.add('contactContainer');
+        const profile = document.createElement('div');
+        profile.classList.add('profile');
+        
 
-    const person = document.createElement('p')
-    person.textContent = `Name: Paul Kims`
-    const position =document.createElement('p');
-    position.textContent = `Position: Manager`
-    const phone = document.createElement('p');
-    phone.textContent = `Phone Number: +555-666-777`
-    const email = document.createElement('p');
-    email.textContent = `Email: manager@restaurant`;
+        const photoHolder = document.createElement('div');
+        let staffPhoto = document.createElement('img');
+        staffPhoto.src = myStaff.photo;
 
-    contactContainer.appendChild(person);
-    contactContainer.appendChild(position);
-    contactContainer.appendChild(phone);
-    contactContainer.appendChild(email);
-    document.body.appendChild(contactContainer);
-    
+        photoHolder.appendChild(staffPhoto);
+
+        const contactCard = document.createElement('div');
+        contactCard.classList.add('contactCard');
+
+        const person = document.createElement('p')
+        person.textContent = `Name: ${myStaff.person}`
+        const position =document.createElement('p');
+        position.textContent = `Position: ${myStaff.position}`
+        const phone = document.createElement('p');
+        phone.textContent = `Phone Number: ${myStaff.phone}`
+        const email = document.createElement('p');
+        email.textContent = `Email: ${myStaff.email}`;
+
+        contactCard.appendChild(person);
+        contactCard.appendChild(position);
+        contactCard.appendChild(phone);
+        contactCard.appendChild(email);
+
+        profile.appendChild(photoHolder);
+        profile.appendChild(contactCard);
+
+        content.appendChild(profile);
+    }  
 } 
